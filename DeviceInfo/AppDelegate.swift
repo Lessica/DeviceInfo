5f3691928d5cc13e5e5782b2ce44455f802e84b9
82Flex <82flex@gmail.com>
2021-06-15 00:29:05 +0800

    Initial Commit

---
+++ DeviceInfo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DeviceInfo
+//
+//  Created by Rachel on 2021/6/15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
