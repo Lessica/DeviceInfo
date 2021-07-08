a5d8368a59c60c248752a46bc80729548d048aba
82Flex <82flex@gmail.com>
2021-07-08 17:33:50 +0800

    Use WTFPL

--- DeviceInfo/Records/Transformers/CPUModelTransformer.swift
+++ DeviceInfo/Records/Transformers/CPUModelTransformer.swift
@@ -1,9 +1,7 @@
-//
-//  CPUModelTransformer.swift
-//  DeviceInfo
-//
-//  Created by Rachel on 2021/7/5.
-//
+//# This project is libre, and licenced under the terms of the
+//# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE, version 3.1,
+//# as published by dtf on July 2019. See the COPYING file or
+//# https://ph.dtf.wtf/w/wtfpl/#version-3-1 for more details.
 
 import Foundation
 
--- DeviceInfo/Resources/BartyCrouch.swift
+++ DeviceInfo/Resources/BartyCrouch.swift
@@ -1,9 +1,7 @@
-//
-//  BartyCrouch.swift
-//  DeviceInfo
-//
-//  Created by Rachel on 2021/7/4.
-//
+//# This project is libre, and licenced under the terms of the
+//# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE, version 3.1,
+//# as published by dtf on July 2019. See the COPYING file or
+//# https://ph.dtf.wtf/w/wtfpl/#version-3-1 for more details.
 
 //  This file is required in order for the `transform` task of the translation helper tool BartyCrouch to work.
 //  See here for more details: https://github.com/Flinesoft/BartyCrouch
